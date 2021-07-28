be5c0baca85d64a7ac64d18329c9e37f547bdd97
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-07-28 18:28:48 -0500

    ignore

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'dist')
